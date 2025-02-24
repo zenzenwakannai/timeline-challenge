40e73aa247bc682de371572138c1bfc2030792a6
Ryan Chen <197658174+zenzenwakannai@users.noreply.github.com>
2025-02-24 18:52:34 +0800

    Remove unused file

--- src/index.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-it("renders welcome message", () => {
-  render(<App />);
-  expect(screen.getByText("Time")).toBeInTheDocument();
-});
